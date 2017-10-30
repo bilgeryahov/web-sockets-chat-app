d6459cfcf36cf92f1766c9eb645a26510c0c510c
Bilger Yahov <bilger@studyportals.com>
2017-10-30 21:05:53 +0100

    Add express as a dependency. Add nodemon as a dev dependency. Initialize the server.

---
+++ web-sockets-chat-app/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+// App setup
+const app = express();
+
+const server = app.listen(4000, function () {
+
+	console.log('Listening to requests on port number 4000....');
+});
